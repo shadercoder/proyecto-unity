94a68f629409aec5f413683102e3f91f642c97a5
elhombrerima <elhombrerima@gmail.com>
2012-01-02 01:21:30 +0000

    Creada una escena inicial como menu y retocados otros metodos dentro de SmoothMouseOrbit.js y Estados.js

---
+++ Proyecto_Unity/Assets/Scripts/Orbita.js
@@ -0,0 +1,24 @@
+#pragma strict
+
+var target : Transform;
+var orbitingBody : Transform;
+var speed : float;
+var orbitDir : Vector3;
+var direction : Vector3;
+
+function Awake(){
+	orbitingBody = transform;
+} 
+
+// Use this for initialization
+function Start() {
+	var line : Vector3 = target.position - transform.position;
+	line.Normalize();
+	Vector3.OrthoNormalize(line, direction, orbitDir); 
+}
+
+// Update is called once per frame
+function Update () {
+	transform.RotateAround (target.position, orbitDir, speed * Time.deltaTime);
+}
+	
