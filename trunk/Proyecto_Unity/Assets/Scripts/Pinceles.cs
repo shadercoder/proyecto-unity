42c84705c7d139972ee6794addf0fa15bcacc700
elhombrerima@gmail.com <elhombrerima@gmail.com@e77a2ff0-ac2f-79f9-5a50-7245fbc8f8d3>
2012-04-03 18:24:08 +0000

    Creados métodos para alterar el terreno con pinceles. Otros arreglos también.

--- trunk/Proyecto_Unity/Assets/Scripts/Pinceles.cs
+++ trunk/Proyecto_Unity/Assets/Scripts/Pinceles.cs
@@ -7,5 +7,6 @@ public class Pinceles : MonoBehaviour {
 	public Texture2D fisura2;
 	public Texture2D volcan1;
 	public Texture2D volcan2;
-	
+	public Texture2D circulo1;
+	public Texture2D circulo2;	
 }
