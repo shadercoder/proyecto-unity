687c9753add6b575baf18fe125489d60e5af5a39
marcoscallejafernandez <marcoscallejafernandez@gmail.com>
2012-07-24 13:30:20 +0000

    Arreglado 1 fallo. Limpieza sustancial de varios scripts.

--- Proyecto_Unity/Assets/Scripts/InterfazPrincipal.cs
+++ Proyecto_Unity/Assets/Scripts/InterfazPrincipal.cs
@@ -321,32 +321,15 @@ public class InterfazPrincipal : MonoBehaviour {
 			
 			if(Input.GetMouseButton(0))
 			{
-				//EspecieAnimal especie = new EspecieAnimal("comemusgo"+estados.vida.numEspeciesAnimales,10,1000,0,5,5,5,tipoAlimentacionAnimal.herbivoro,T_habitats.plain, estados.carnivoro1);
-				//estados.vida.anadeEspecieAnimal(especie);						
 				RaycastHit hit;
 				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 				if (estados.objetoRoca.collider.Raycast(ray, out hit, Mathf.Infinity)) 
 				{
-					/*Vector2 coordTemp = hit.textureCoord;
-					Texture2D tex = estados.objetoRoca.renderer.sharedMaterial.mainTexture as Texture2D;
-					coordTemp.x = (int)((int)(coordTemp.x * tex.width) / FuncTablero.getRelTexTabAncho());
-					coordTemp.y = (int)((int)(coordTemp.y * tex.height) / FuncTablero.getRelTexTabAlto());
-					Casilla elem = estados.vida.tablero[(int)coordTemp.y, (int)coordTemp.x];*/	
 					double xTemp = hit.textureCoord.x;
 					double yTemp = hit.textureCoord.y;
 					Texture2D tex = estados.objetoRoca.renderer.sharedMaterial.mainTexture as Texture2D;
 					xTemp = xTemp * tex.width/ FuncTablero.getRelTexTabAncho();
-					//double relTexTabAlto = FuncTablero.altoTextura / FuncTablero.altoTableroUtil;
-					//yTemp = (yTemp * tex.height/ relTexTabAlto) - 1;// - 2;
 					yTemp = (yTemp * tex.height/ FuncTablero.getRelTexTabAlto()) - 0.5f;
-					/*if(xTemp-0.5 < (int)xTemp)
-						xTemp = System.Math.Floor(xTemp);
-					else
-						xTemp = System.Math.Ceiling(xTemp);
-					if(yTemp-0.5 < (int)yTemp)
-						yTemp = System.Math.Floor(yTemp);
-					else
-						yTemp = System.Math.Ceiling(yTemp);*/
 					int x = (int)xTemp;
 					int y = (int)yTemp;
 					FuncTablero.convierteCoordenadas(ref y, ref x);
@@ -367,8 +350,8 @@ public class InterfazPrincipal : MonoBehaviour {
 						estados.vida.especies.Values.CopyTo(especies,0);						
 						EspecieVegetal especie = (EspecieVegetal)especies[tipo];
 						estados.vida.anadeVegetal(especie,y,x);
-						//elementoInsercion = telementoInsercion.ninguno;
-						//accion = taccion.ninguna;
+						elementoInsercion = telementoInsercion.ninguno;
+						accion = taccion.ninguna;
 					}
 					else if(tipo >= 15 && tipo < 25)		//Animal (herbivoro o carnivoro)
 					{
@@ -383,115 +366,7 @@ public class InterfazPrincipal : MonoBehaviour {
 					else
 						return;
 				}				
-//				if(elementoInsercion == telementoInsercion.ninguno)
-//					return;
-//				RaycastHit hit;
-//				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//				if (estados.objetoRoca.collider.Raycast(ray, out hit, Mathf.Infinity)) 
-//				{
-//					Vector2 coordTemp = hit.textureCoord;
-//					Texture2D tex = estados.objetoRoca.renderer.sharedMaterial.mainTexture as Texture2D;
-//					coordTemp.x = (int)((int)(coordTemp.x * tex.width) / FuncTablero.getRelTexTabAncho());
-//					coordTemp.y = (int)((int)(coordTemp.y * tex.height) / FuncTablero.getRelTexTabAlto());
-//					int x = (int)coordTemp.x;
-//					int y = (int)coordTemp.y; 
-//					FuncTablero.convierteCoordenadas(ref x,ref y);
-//					//Casilla elem = estados.vida.tablero[(int)coordTemp.x,(int)coordTemp.y];					
-//					Casilla elem = estados.vida.tablero[x,y];					
-//					int tipo = (int)elementoInsercion - 1;
-//					if(tipo >= 0 && tipo < 5)				//Edificio
-//					{
-//						/*TipoEdificio[] tipos = new TipoEdificio[estados.vida.tiposEdificios.Count];
-//						estados.vida.tiposEdificios.Values.CopyTo(tipos,0);
-//						TipoEdificio tedif = tipos[tipo];
-//						List<T_habitats> habitats1 = new List<T_habitats>();
-//						habitats1.Add(T_habitats.plain);
-//						habitats1.Add(T_habitats.sand);
-//						habitats1.Add(T_habitats.coast);
-//						EspecieVegetal especieV1 = new EspecieVegetal("vegetal1",1000,50,50,50,0.1f,8,habitats1,0,estados.vegetal1);
-//						estados.vida.anadeVegetal(especieV1,x,y);
-//						
-//						//elementoInsercion = telementoInsercion.ninguno;
-//						//accion = taccion.ninguna;
-//						
-//					}/*
-//					else if(tipo >= 5 && tipo < 15)			//Vegetal
-//					{
-//						tipo -= 5;
-//						Especie[] especies = new Especie[estados.vida.especies.Count];
-//						estados.vida.especies.Values.CopyTo(especies,0);						
-//						EspecieVegetal especie = (EspecieVegetal)especies[tipo];
-//						estados.vida.anadeVegetal(especie,x,y);
-//						
-//					}
-//					else if(tipo >= 15 && tipo < 25)				//Animal (herbivoro o carnivoro)
-//					{
-//						tipo -= 5;	
-//						Especie[] especies = new Especie[estados.vida.especies.Count];
-//						estados.vida.especies.Values.CopyTo(especies,0);
-//						EspecieAnimal especie = (EspecieAnimal)especies[tipo];
-//						estados.vida.anadeAnimal(especie,x,y);						
-//					}
-//					else
-//						return;
-//				}*/								
-//				/*switch (elementoInsercion)
-//				{
-//					case telementoInsercion.ninguno:						
-//						return;
-//					case telementoInsercion.fabricaCompBas:
-//						
-//						break;
-//					case telementoInsercion.centralEnergia:
-//						break;
-//					case telementoInsercion.granja:
-//						break;
-//					case telementoInsercion.fabricaCompAdv:
-//						break;
-//					case telementoInsercion.centralEnergiaAdv:
-//						break;
-//					case telementoInsercion.vegetal1:
-//						break;
-//					case telementoInsercion.vegetal2:
-//						break;
-//					case telementoInsercion.vegetal3:
-//						break;
-//					case telementoInsercion.vegetal4:
-//						break;
-//					case telementoInsercion.vegetal5:
-//						break;
-//					case telementoInsercion.vegetal6:
-//						break;
-//					case telementoInsercion.vegetal7:
-//						break;
-//					case telementoInsercion.vegetal8:
-//						break;
-//					case telementoInsercion.vegetal9:
-//						break;
-//					case telementoInsercion.vegetal10:
-//						break;
-//					case telementoInsercion.herbivoro1:
-//						break;
-//					case telementoInsercion.herbivoro2:
-//						break;
-//					case telementoInsercion.herbivoro3:
-//						break;
-//					case telementoInsercion.herbivoro4:
-//						break;
-//					case telementoInsercion.herbivoro5:
-//						break;
-//					case telementoInsercion.carnivoro1:
-//						break;
-//					case telementoInsercion.carnivoro2:
-//						break;
-//					case telementoInsercion.carnivoro3:
-//						break;
-//					case telementoInsercion.carnivoro4:
-//						break;
-//					case telementoInsercion.carnivoro5:
-//						break;
-//					default:break;
-//				}*/								
+							
 			}			
 		}
 	}
@@ -527,182 +402,5 @@ public class InterfazPrincipal : MonoBehaviour {
 			GUI.Box(pos, "");
 			GUI.Label(pos, GUI.tooltip);
 		}
-	}
-	
-	/*
-	// Interfaz para aspect ratio 16:10
-	void interfaz16_10()
-	{
-		int temp = 0;		
-		cuantoW	= (float)Screen.width / 80;
-		cuantoH	= (float)Screen.height / 50;
-		
-		//bloque superior
-		GUI.BeginGroup(new Rect(cuantoW*0,cuantoH*0,cuantoW*80,cuantoH*3));
-		GUI.Box(new Rect(cuantoW*0,cuantoH*0,cuantoW*73,cuantoH*3),"","BloqueSuperior");		
-		GUI.Label(new Rect(cuantoW*2,cuantoH*0,cuantoW*6,cuantoH*2),estados.numPasos.ToString(),"EtiquetaTiempo");
-		if(GUI.Button(new Rect(cuantoW*4,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Pausa el juego"),"BotonPausa"))
-			Time.timeScale = 0;//estados.escalaTiempo = 0;	
-		if(GUI.Button(new Rect(cuantoW*5,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad normal"),"BotonVelocidad1"))
-			Time.timeScale = 1;//estados.escalaTiempo = 1;	
-		if(GUI.Button(new Rect(cuantoW*6,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 2x"),"BotonVelocidad2"))
-			Time.timeScale = 2;//estados.escalaTiempo = 2;	
-		if(GUI.Button(new Rect(cuantoW*7,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 5x"),"BotonVelocidad5"))
-			Time.timeScale = 5;//estados.escalaTiempo = 5;	
-		if(GUI.Button(new Rect(cuantoW*73,cuantoH*0,cuantoW*7,cuantoH*3),new GUIContent("","Accede al menu del juego"),"BotonMenu"))
-			temp++;		
-		GUI.EndGroup();
-		
-		//bloque izquierdo
-		GUILayout.BeginArea(new Rect(cuantoW*0,cuantoH*20,cuantoW*3,cuantoH*10),new GUIContent(),"BloqueIzquierdo");		
-		GUILayout.BeginHorizontal();
-		if(mostrarBloqueIzquierdo)
-		{
-			GUILayout.BeginVertical(GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*2));			
-			if(GUILayout.Button(new GUIContent("","Accede al menu de insertar animales o vegetales"),"BotonInsertarVida"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de construir edificios"),"BotonInsertarEdificios"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de mejoras de la nave"),"BotonAccederMejoras"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de habilidades"),"BotonAccederHabilidades"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Cambia entre info y seleccionar"),"BotonInfoSelec"))
-				temp++;					
-			GUILayout.EndVertical();
-			if(GUILayout.Button(new GUIContent("","Pulsa para ocultar este menu"),"BotonOcultarBloqueIzquierdo",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = false;
-		}
-		else
-			if(GUILayout.Button(new GUIContent("","Pulsa para mostrar el menu de acciones"),"BotonOcultarBloqueIzquierdoActivado",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = true;	
-			
-		GUILayout.EndHorizontal();		
-		GUILayout.EndArea();
-		
-		//Barra construccion
-		GUI.BeginGroup(new Rect(cuantoW*22,cuantoH*45,cuantoW*36,cuantoH*4),"","BloqueConstruccion");
-		GUI.EndGroup();
-		
-		//Barra construccion
-		GUI.Box(new Rect(cuantoW*0,cuantoH*49,cuantoW*100,cuantoH*1),"","BloqueInferior");	
-	}
-	
-	// Interfaz para aspect ratio 16:9
-	void interfaz16_9()
-	{
-		int temp = 0;		
-		cuantoW	= (float)Screen.width / 80;
-		cuantoH	= (float)Screen.height / 45;
-		
-		Estados estados = gameObject.GetComponent<Estados>();		
-		//bloque superior
-		GUI.BeginGroup(new Rect(cuantoW*0,cuantoH*0,cuantoW*80,cuantoH*3));
-		GUI.Box(new Rect(cuantoW*0,cuantoH*0,cuantoW*73,cuantoH*3),"","BloqueSuperior");		
-		GUI.Label(new Rect(cuantoW*2,cuantoH*0,cuantoW*6,cuantoH*2),estados.numPasos.ToString(),"EtiquetaTiempo");
-		if(GUI.Button(new Rect(cuantoW*4,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Pausa el juego"),"BotonPausa"))
-			Time.timeScale = 0;//estados.escalaTiempo = 0;	
-		if(GUI.Button(new Rect(cuantoW*5,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad normal"),"BotonVelocidad1"))
-			Time.timeScale = 1;//estados.escalaTiempo = 1;	
-		if(GUI.Button(new Rect(cuantoW*6,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 2x"),"BotonVelocidad2"))
-			Time.timeScale = 2;//estados.escalaTiempo = 2;	
-		if(GUI.Button(new Rect(cuantoW*7,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 5x"),"BotonVelocidad5"))
-			Time.timeScale = 5;//estados.escalaTiempo = 5;	
-		if(GUI.Button(new Rect(cuantoW*73,cuantoH*0,cuantoW*7,cuantoH*3),new GUIContent("","Accede al menu del juego"),"BotonMenu"))
-			temp++;		
-		GUI.EndGroup();
-		
-		//bloque izquierdo
-		GUILayout.BeginArea(new Rect(cuantoW*0,cuantoH*17,cuantoW*3,cuantoH*10),new GUIContent(),"BloqueIzquierdo");		
-		GUILayout.BeginHorizontal();
-		if(mostrarBloqueIzquierdo)
-		{
-			GUILayout.BeginVertical(GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*2));			
-			if(GUILayout.Button(new GUIContent("","Accede al menu de insertar animales o vegetales"),"BotonInsertarVida"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de construir edificios"),"BotonInsertarEdificios"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de mejoras de la nave"),"BotonAccederMejoras"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de habilidades"),"BotonAccederHabilidades"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Cambia entre info y seleccionar"),"BotonInfoSelec"))
-				temp++;					
-			GUILayout.EndVertical();
-			if(GUILayout.Button(new GUIContent("","Pulsa para ocultar este menu"),"BotonOcultarBloqueIzquierdo",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = false;
-		}
-		else
-			if(GUILayout.Button(new GUIContent("","Pulsa para mostrar el menu de acciones"),"BotonOcultarBloqueIzquierdoActivado",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = true;	
-			
-		GUILayout.EndHorizontal();		
-		GUILayout.EndArea();
-		
-		//Barra construccion
-		GUI.BeginGroup(new Rect(cuantoW*22,cuantoH*40,cuantoW*36,cuantoH*4),"","BloqueConstruccion");
-		GUI.EndGroup();
-		
-		//Barra construccion
-		GUI.Box(new Rect(cuantoW*0,cuantoH*44,cuantoW*100,cuantoH*1),"","BloqueInferior");		
-	}
-	
-	// Interfaz para aspect ratio 4:3
-	void interfaz4_3()
-	{
-		int temp = 0;		
-		cuantoW	= (float)Screen.width / 80;
-		cuantoH	= (float)Screen.height / 60;
-		
-		Estados estados = gameObject.GetComponent<Estados>();		
-		//bloque superior
-		GUI.BeginGroup(new Rect(cuantoW*0,cuantoH*0,cuantoW*80,cuantoH*3));
-		GUI.Box(new Rect(cuantoW*0,cuantoH*0,cuantoW*73,cuantoH*3),"","BloqueSuperior");		
-		GUI.Label(new Rect(cuantoW*2,cuantoH*0,cuantoW*6,cuantoH*2),estados.numPasos.ToString(),"EtiquetaTiempo");
-		if(GUI.Button(new Rect(cuantoW*4,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Pausa el juego"),"BotonPausa"))
-			Time.timeScale = 0;//estados.escalaTiempo = 0;	
-		if(GUI.Button(new Rect(cuantoW*5,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad normal"),"BotonVelocidad1"))
-			Time.timeScale = 1;//estados.escalaTiempo = 1;	
-		if(GUI.Button(new Rect(cuantoW*6,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 2x"),"BotonVelocidad2"))
-			Time.timeScale = 2;//estados.escalaTiempo = 2;	
-		if(GUI.Button(new Rect(cuantoW*7,cuantoH*2,cuantoW*1,cuantoH*1),new GUIContent("","Velocidad 5x"),"BotonVelocidad5"))
-			Time.timeScale = 5;//estados.escalaTiempo = 5;	
-		if(GUI.Button(new Rect(cuantoW*73,cuantoH*0,cuantoW*7,cuantoH*3),new GUIContent("","Accede al menu del juego"),"BotonMenu"))
-			temp++;		
-		GUI.EndGroup();
-		
-		//bloque izquierdo
-		GUILayout.BeginArea(new Rect(cuantoW*0,cuantoH*25,cuantoW*3,cuantoH*10),new GUIContent(),"BloqueIzquierdo");		
-		GUILayout.BeginHorizontal();
-		if(mostrarBloqueIzquierdo)
-		{
-			GUILayout.BeginVertical(GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*2));			
-			if(GUILayout.Button(new GUIContent("","Accede al menu de insertar animales o vegetales"),"BotonInsertarVida"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de construir edificios"),"BotonInsertarEdificios"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de mejoras de la nave"),"BotonAccederMejoras"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Accede al menu de habilidades"),"BotonAccederHabilidades"))
-				temp++;		
-			if(GUILayout.Button(new GUIContent("","Cambia entre info y seleccionar"),"BotonInfoSelec"))
-				temp++;					
-			GUILayout.EndVertical();
-			if(GUILayout.Button(new GUIContent("","Pulsa para ocultar este menu"),"BotonOcultarBloqueIzquierdo",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = false;
-		}
-		else
-			if(GUILayout.Button(new GUIContent("","Pulsa para mostrar el menu de acciones"),"BotonOcultarBloqueIzquierdoActivado",GUILayout.Height(cuantoH*10),GUILayout.Width(cuantoH*1)))
-				mostrarBloqueIzquierdo = true;	
-			
-		GUILayout.EndHorizontal();		
-		GUILayout.EndArea();
-		
-		//Barra construccion
-		GUI.BeginGroup(new Rect(cuantoW*22,cuantoH*55,cuantoW*36,cuantoH*4),"","BloqueConstruccion");
-		GUI.EndGroup();
-		
-		//Barra construccion
-		GUI.Box(new Rect(cuantoW*0,cuantoH*59,cuantoW*100,cuantoH*1),"","BloqueInferior");
-	}*/	
+	}	
 }
