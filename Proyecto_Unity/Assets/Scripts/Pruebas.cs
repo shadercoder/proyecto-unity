ce844374c8a3f57401c202e49e126e00dd7c4419
elhombrerima <elhombrerima@gmail.com>
2012-08-09 10:45:10 +0000

    Mejoras en el control de la nave y su camara, en la escena de pruebas y en las mejoras de la nave. Ahora la orbita cambia suavemente cuando se mejora.

--- Proyecto_Unity/Assets/Scripts/Pruebas.cs
+++ Proyecto_Unity/Assets/Scripts/Pruebas.cs
@@ -2,25 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class Pruebas : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 	
 	void OnGUI() {
-		if (GUI.Button(new Rect(50, 30, 70, 20), "Mejora1")) {
+		if (GUI.Button(new Rect(50, 30, 70, 20), "Speed1")) {
 			MejorasNave temp = GameObject.FindGameObjectWithTag("Mejoras").GetComponent<MejorasNave>();
 			temp.compraMejora1();
 		}
-		if (GUI.Button(new Rect(150, 30, 70, 20), "Mejora2")) {
+		if (GUI.Button(new Rect(150, 30, 70, 20), "Speed2")) {
 			MejorasNave temp = GameObject.FindGameObjectWithTag("Mejoras").GetComponent<MejorasNave>();
 			temp.compraMejora2();
+		}
+		
+		if (GUI.Button(new Rect(250, 30, 70, 20), "Orbita")) {
+			MejorasNave temp = GameObject.FindGameObjectWithTag("Mejoras").GetComponent<MejorasNave>();
+			temp.compraMejora7();
 		}
 	}
 }
