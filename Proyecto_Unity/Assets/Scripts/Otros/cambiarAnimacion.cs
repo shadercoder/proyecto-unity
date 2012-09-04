8b314af41093bff18fa29c62d1217f31efd1aacf
elhombrerima <elhombrerima@gmail.com>
2012-09-04 17:23:11 +0000

    Adaptados los prefabs para los efectos de particulas.

--- Proyecto_Unity/Assets/Scripts/Otros/cambiarAnimacion.cs
+++ Proyecto_Unity/Assets/Scripts/Otros/cambiarAnimacion.cs
@@ -5,8 +5,28 @@ public class cambiarAnimacion : MonoBehaviour {
 
 	public AnimationClip animacion;
 	
-	void Play ()
+	public GameObject particulasDormir;
+	public GameObject particulasComer;
+	public GameObject particulasMorir;
+	
+	public void Play ()
 	{
-	    transform.animation.Play(animacion.name);
+		if (animacion != null)
+	    	transform.animation.Play(animacion.name);
+	}
+	
+	public void playParticulasDormir() {
+		if (particulasDormir != null)
+			Instantiate(particulasDormir, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+	
+	public void playParticulasComer() {
+		if (particulasComer != null)
+			Instantiate(particulasComer, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+	
+	public void playParticulasMorir() {
+		if (particulasMorir != null)
+			Instantiate(particulasMorir, this.gameObject.transform.position, this.gameObject.transform.rotation);
 	}
 }
